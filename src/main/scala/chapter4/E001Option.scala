9a8d6e270f74d65fc6c86c753abeb999820ba088
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-03-20 00:40:29 +0530

    chapter4: add flatmap for option

--- src/main/scala/chapter4/E001Option.scala
+++ src/main/scala/chapter4/E001Option.scala
@@ -1,12 +1,19 @@
 package chapter4
 
 object E001Option {
-  trait Option[+A] {
+  sealed trait Option[+A] {
     def map[B](f: A => B): Option[B] =
       this match {
         case Some(value: A) => Some(f(value))
         case None           => None
       }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = {
+      this match {
+        case Some(value: A) => f(value)
+        case None           => None
+      }
+    }
   }
 
   case class Some[+A](value: A) extends Option[A]
--- src/test/scala/chapter4/E001OptionTest.scala
+++ src/test/scala/chapter4/E001OptionTest.scala
@@ -13,4 +13,19 @@ class E001OptionTest extends FunSuite {
     val some = Some(3)
     assertEquals(some.map(x => (x * 2).toString), Some("6"))
   }
+
+  test("flatMap should return None for initial None") {
+    val none = None
+    assertEquals(none.flatMap(_ => Some(4)), None)
+  }
+
+  test("flatMap should return None for next None") {
+    val some = Some(4)
+    assertEquals(some.flatMap(i => None), None)
+  }
+
+  test("flatMap should return Some for both Some") {
+    val some = Some(4)
+    assertEquals(some.flatMap(i => Some(i + 1)), Some(5))
+  }
 }
