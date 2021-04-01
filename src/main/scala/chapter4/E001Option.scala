7c4de54e16180a0731b5b4ee3d5a40046948c8ca
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-04-01 22:25:55 +0530

    implement orElse and getOrElse ;
    remove pattern matching

--- src/main/scala/chapter4/E001Option.scala
+++ src/main/scala/chapter4/E001Option.scala
@@ -3,22 +3,29 @@ package chapter4
 object E001Option {
   sealed trait Option[+A] {
     def map[B](f: A => B): Option[B] =
-      this match {
-        case Some(value: A) => Some(f(value))
-        case None           => None
-      }
+      if (isDefined) Some(f(get)) else None
 
-    def flatMap[B](f: A => Option[B]): Option[B] = {
-      this match {
-        case Some(value: A) => f(value)
-        case None           => None
-      }
-    }
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      if (isDefined) f(get) else None
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+      if (isDefined) this else ob
+
+    def getOrElse[B >: A](default: => B): B = if (isDefined) get else default
 
     def filter(f: A => Boolean): Option[A] =
       this.flatMap(a => if (f(a)) this else None)
+
+    val isDefined: Boolean
+    def get: A
   }
 
-  case class Some[+A](value: A) extends Option[A]
-  case object None              extends Option[Nothing]
+  case class Some[+A](value: A) extends Option[A] {
+    override val isDefined: Boolean = true
+    override lazy val get: A        = value
+  }
+  case object None extends Option[Nothing] {
+    override val isDefined: Boolean = false
+    override lazy val get: Nothing  = throw new RuntimeException("no value")
+  }
 }
--- src/test/scala/chapter4/E001OptionTest.scala
+++ src/test/scala/chapter4/E001OptionTest.scala
@@ -9,38 +9,73 @@ class E001OptionTest extends FunSuite {
     assertEquals(none.map(_ => 4), None)
   }
 
-  test("map should return map a some object") {
+  test("map should return new value for Some ") {
     val some = Some(3)
     assertEquals(some.map(x => (x * 2).toString), Some("6"))
   }
 
-  test("flatMap should return None for initial None") {
+  test("flatMap should return None for None-Some") {
     val none = None
     assertEquals(none.flatMap(_ => Some(4)), None)
   }
 
-  test("flatMap should return None for next None") {
+  test("flatMap should return None for None-None") {
     val some = Some(4)
     assertEquals(some.flatMap(i => None), None)
   }
 
-  test("flatMap should return Some for both Some") {
+  test("flatMap should return Some for Some-Some") {
     val some = Some(4)
     assertEquals(some.flatMap(i => Some(i + 1)), Some(5))
   }
 
-  test("filter should return None for false and None") {
+  test("flatMap should return None for Some-None") {
+    val some = Some(4)
+    assertEquals(some.flatMap(i => None), None)
+  }
+
+  test("filter None-false = None") {
     val none: Option[Int] = None
     assertEquals(none.filter(_ > 0), None)
   }
 
-  test("filter should return Some for true and Some") {
-    val none: Option[Int] = Some(3)
-    assertEquals(none.filter(_ > 0), Some(3))
+  test("filter Some-true = Some") {
+    val some: Option[Int] = Some(3)
+    assertEquals(some.filter(_ > 0), Some(3))
   }
 
-  test("filter should return None for false and Some") {
+  test("filter Some-false = None") {
     val none: Option[Int] = Some(3)
     assertEquals(none.filter(_ < 0), None)
+  }
+
+  test("getOrElse None = new value") {
+    val none: Option[Int] = None
+    assertEquals(none.getOrElse(3), 3)
+  }
+
+  test("getOrElse Some = previous value") {
+    val some: Option[Int] = Some(3)
+    assertEquals(some.getOrElse(5), 3)
+  }
+
+  test("orElse Some A - Some B = Some A") {
+    val some: Option[Int] = Some(3)
+    assertEquals(some.orElse(Some(5)), Some(3))
+  }
+
+  test("orElse Some A - None = Some A") {
+    val some: Option[Int] = Some(3)
+    assertEquals(some.orElse(None), Some(3))
+  }
+
+  test("orElse None - Some B = Some B") {
+    val none: Option[Int] = None
+    assertEquals(none.orElse(Some(5)), Some(5))
+  }
+
+  test("orElse None - None = None") {
+    val none: Option[Int] = None
+    assertEquals(none.orElse(None), None)
   }
 }
